534f992cbd9995f60a3342a8b0187dd3d7887000
Alexander Pershukevich <apershuk@uwaterloo.ca>
2026-09-11 21:28:45 -0400

    hmm

--- src/submission.hpp
+++ src/submission.hpp
@@ -11,8 +11,7 @@ static constexpr T* assume_aligned(T* ptr) noexcept {
 }
 
 
-class Grid 
-{
+class Grid {
 private:
     std::size_t rows_;
     std::size_t cols_;
@@ -47,9 +46,8 @@ public:
 };
 
 void copy_border(const Grid& old_grid, Grid& new_grid, const std::size_t& rows,
-                        const std::size_t& cols, const std::size_t row_width) 
+                const std::size_t& cols, const std::size_t row_width)
 {
-
     const double* __restrict src= old_grid.data();
     double* __restrict dst= new_grid.data();
 
@@ -64,11 +62,10 @@ void copy_border(const Grid& old_grid, Grid& new_grid, const std::size_t& rows,
 
     }
 
+
 }
 
-void apply_stencil(const Grid& old_grid, Grid& new_grid) 
-{ 
-    
+void apply_stencil(const Grid& old_grid, Grid& new_grid) {
     const std::size_t rows= old_grid.row_size();
     const std::size_t cols= old_grid.col_size();
     const size_t row_width= old_grid.row_width();
@@ -91,5 +88,5 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid)
     }
 }
     /// border
-   copy_border(old_grid, new_grid,rows, cols, row_width);
+    copy_border(old_grid, new_grid,rows, cols, row_width);
 }
